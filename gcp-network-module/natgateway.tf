bdd832dab0305de47d768f2e7e1e8f359902f874
Mostafa Ahmed <mostafa.97ahmed.ma@gmail.com>
2022-10-27 21:42:55 +0200

    final commit

--- gcp-network-module/natgateway.tf
+++ gcp-network-module/natgateway.tf
@@ -1,21 +1,19 @@
 resource "google_compute_router" "router" {
   name    = "nat-router"
-  region  = google_compute_subnetwork.subnet.region
+  region  = google_compute_subnetwork.subnet1.region
   network = google_compute_network.task-vpc.id
 
-  bgp {
-    asn = 64514
-  }
 }
 resource "google_compute_router_nat" "nat-gw" {
   name                               = var.nat-name
   router                             = google_compute_router.router.name
   region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
+  #source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.subnet2.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
+}
 }
